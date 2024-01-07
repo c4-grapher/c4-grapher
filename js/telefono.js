0ec84ab65abf906fa1118c01b7205372c0f49049
OsalexRod <alejandrorodriguezsalazar997@gmail.com>
2024-01-06 20:51:58 -0400

    mejorando la clase telefono

--- js/telefono.js
+++ js/telefono.js
@@ -1,15 +1,18 @@
 class Telefono extends Figura {
-    constructor(x, y) {
-        super(x, y);
+    constructor(x, y, estrategiaColor) {
+        super(x, y, estrategiaColor);
     }
 
     draw() {
         push();
-        fill(8, 66, 123);
+        noStroke();
+        fill(this.estrategiaColor.getColor());
         rect(this.x, this.y, this.ancho, this.alto, 5);
+        fill(this.estrategiaColor.getColorClaro());
         circle(this.x + 10, this.centroY, 9);
-        rect(this.x + 20, this.y + 6, this.ancho - 30, this.alto - 11, 5);
-        
+        rect(this.x + 20, this.y + 6, this.ancho - 35, this.alto - 11, 5);
+        stroke(this.estrategiaColor.getColorClaro());
+        line(this.x + this.ancho - 7, this.y + (this.alto * 0.33), this.x + this.ancho - 7, this.y + (this.alto * 0.66)); 
         super.dibujarTexto();
         super.dibujarCajaDeMovimiento();
         pop();
